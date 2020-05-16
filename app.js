3486db82fbd1c14fef6dc945c1a25b7104c33f78
mryanshenghong <mryanshenghong@gmail.com>
2020-05-16 03:08:12 -0400

    增加移动端拖拽

--- app.js
+++ app.js
@@ -1,94 +1,91 @@
 // initialize current page
-var current = 'browse';
+var current = 'browse'
 // play mode
-var currentPlayMode = 'normal';
+var currentPlayMode = 'normal'
 
-var playerHide = true;
+var playerHide = true
 // player setup
-var player = document.getElementById('player');
+var player = document.getElementById('player')
 
 var isStop = true
 // music list
-var currentIndex = 0;
+var currentIndex = 0
 var allTracks = [
-	{
-		img:'./img/songsImg/faded.jpg',
-		url:'./songs/song1.mp3',
-		name:'Faded',
-		singer:'Alan Walker',
-		album:'Different World',
-		duration:'3:33'
-	},
-	{
-		img:'./img/songsImg/autumn.jpg',
-		url:'./songs/song2.mp3',
-		name:'Autumn Leaves',
-		singer:'Unknown',
-		album:'Instrument',
-		duration:'7:01'
-	},
-	{
-		img:'./img/songsImg/slamdumk.jpg',
-		url:'./songs/song3.mp3',
-		name:'Slam Dumk',
-		singer:'Unknown',
-		album:'Instrument',
-		duration:'2:37'
-	},
-	{
-		img:'./img/songsImg/cherry.jpg',
-		url:'./songs/song4.mp3',
-		name:'Cherry blossom time',
-		singer:'Kotaro Oshio',
-		album:'Unknown',
-		duration:'3:39'
-	},
-	{
-		img:'./img/songsImg/lovesong.jpg',
-		url:'./songs/song5.mp3',
-		name:'情歌',
-		singer:'梁静茹',
-		album:'静茹&情歌',
-		duration:'4:18'
-	}	
-];
+  {
+    img: './img/songsImg/faded.jpg',
+    url: './songs/song1.mp3',
+    name: 'Faded',
+    singer: 'Alan Walker',
+    album: 'Different World',
+    duration: '3:33',
+  },
+  {
+    img: './img/songsImg/autumn.jpg',
+    url: './songs/song2.mp3',
+    name: 'Autumn Leaves',
+    singer: 'Unknown',
+    album: 'Instrument',
+    duration: '7:01',
+  },
+  {
+    img: './img/songsImg/slamdumk.jpg',
+    url: './songs/song3.mp3',
+    name: 'Slam Dumk',
+    singer: 'Unknown',
+    album: 'Instrument',
+    duration: '2:37',
+  },
+  {
+    img: './img/songsImg/cherry.jpg',
+    url: './songs/song4.mp3',
+    name: 'Cherry blossom time',
+    singer: 'Kotaro Oshio',
+    album: 'Unknown',
+    duration: '3:39',
+  },
+  {
+    img: './img/songsImg/lovesong.jpg',
+    url: './songs/song5.mp3',
+    name: '情歌',
+    singer: '梁静茹',
+    album: '静茹&情歌',
+    duration: '4:18',
+  },
+]
 
-player.src = allTracks[currentIndex].url;
+player.src = allTracks[currentIndex].url
 
 /*
 	show current page 
 */
 
 function showCurrentPage(currentPage) {
-	switchCurrentPageName(currentPage);
-	let pageArr = document.getElementsByClassName('page');
-	for (item of pageArr) {
-		item.style.display = 'none';
-	}
-	let current = document.getElementById(currentPage);
-	if (current) {
-		current.style.transform = 'scale(0)';
-		current.style.display = 'block';
-		setTimeout(() => {
-			current.style.transform = 'scale(1)';
-		}, 300)
-	}
+  switchCurrentPageName(currentPage)
+  let pageArr = document.getElementsByClassName('page')
+  for (item of pageArr) {
+    item.style.display = 'none'
+  }
+  let current = document.getElementById(currentPage)
+  if (current) {
+    current.style.transform = 'scale(0)'
+    current.style.display = 'block'
+    setTimeout(() => {
+      current.style.transform = 'scale(1)'
+    }, 300)
+  }
 }
 
 function switchCurrentPageName(currentPageName) {
-	if(!playerHide) hidePlayer()
-	document.getElementById('currentPageName').innerText = currentPageName.toUpperCase();
+  if (!playerHide) hidePlayer()
+  document.getElementById('currentPageName').innerText = currentPageName.toUpperCase()
 }
 
-showCurrentPage(current);
-
+showCurrentPage(current)
 
 function getClass(className) {
-	return document.getElementsByClassName(className)[0];
+  return document.getElementsByClassName(className)[0]
 }
-
 
 function getId(idName) {
-	return document.getElementById(idName)
+  return document.getElementById(idName)
 }
-
--- player.js
+++ player.js
@@ -1,167 +1,188 @@
-player.oncanplay = function() {
-	generateSongInfo()
-	var duration_m = Math.floor(this.duration / 60);
-	var duration_s = (this.duration - duration_m * 60).toFixed(0);
-	document.getElementById('duration').innerText = duration_m + ':' + ((duration_s < 10) ? '0' + duration_s : duration_s);
+player.oncanplay = function () {
+  generateSongInfo()
+  var duration_m = Math.floor(this.duration / 60)
+  var duration_s = (this.duration - duration_m * 60).toFixed(0)
+  document.getElementById('duration').innerText = duration_m + ':' + (duration_s < 10 ? '0' + duration_s : duration_s)
 }
 timeListener = player.addEventListener('timeupdate', shouCurrentTime)
 endedListener = player.addEventListener('ended', resetPlayer)
 
 function hidePlayer() {
-	var playerMax = document.getElementById('player-max');
-	var playerMin = document.getElementById('player-min');
-	if (!playerHide) {
-		playerMax.style.transform = 'scale(0)';
-		playerMin.style.transform = 'scale(1)';
-		playerHide = true;
-	} else {
-		playerMax.style.transform = 'scale(1)';
-		playerMin.style.transform = 'scale(0)';
-		playerHide = false;
-	}
+  var playerMax = document.getElementById('player-max')
+  var playerMin = document.getElementById('player-min')
+  if (!playerHide) {
+    playerMax.style.transform = 'scale(0)'
+    playerMin.style.transform = 'scale(1)'
+    playerHide = true
+  } else {
+    playerMax.style.transform = 'scale(1)'
+    playerMin.style.transform = 'scale(0)'
+    playerHide = false
+  }
 }
 
 function playSong(index) {
-	if(index === undefined || index === currentIndex) {
-		isStop ? player.play() : player.pause()
-		playState(isStop);		
-	}else {
-		playState(false);
-		currentIndex = index;
-		generateSongInfo();
-		player.src = allTracks[index].url;
-		playSong();
-	}
-	renderPlayList()
+  if (index === undefined || index === currentIndex) {
+    isStop ? player.play() : player.pause()
+    playState(isStop)
+  } else {
+    playState(false)
+    currentIndex = index
+    generateSongInfo()
+    player.src = allTracks[index].url
+    playSong()
+  }
+  renderPlayList()
 }
 
 function resetPlayer() {
-	this.currentTime = 0;
-	getClass('album-img').classList.remove('img-playing');
-	getClass('album-img-min').classList.remove('img-playing');
-	getId('playBtnImg').src = './img/play.svg';
-	next();
+  this.currentTime = 0
+  getClass('album-img').classList.remove('img-playing')
+  getClass('album-img-min').classList.remove('img-playing')
+  getId('playBtnImg').src = './img/play.svg'
+  next()
 }
 
 function shouCurrentTime() {
-	let duration = this.duration
-	var tik = this.currentTime
-	let ratio = tik / duration
-	let totalWidth = getClass('progress-bar').clientWidth
-	getClass('bar-control').style.left = ((totalWidth * ratio) - 5) + 'px'
-	var current_m = parseInt(tik / 60);
-	var current_s = parseInt(tik % 60);
-	document.getElementById('currentTime').innerText = current_m + ':' + ((current_s < 10) ? '0' + current_s : current_s);
+  let duration = this.duration
+  var tik = this.currentTime
+  let ratio = tik / duration
+  let totalWidth = getClass('progress-bar').clientWidth
+  getClass('bar-control').style.left = totalWidth * ratio - 5 + 'px'
+  var current_m = parseInt(tik / 60)
+  var current_s = parseInt(tik % 60)
+  document.getElementById('currentTime').innerText = current_m + ':' + (current_s < 10 ? '0' + current_s : current_s)
 }
 
 function playState(flag) {
-	if (flag) {
-		isStop = false
-		getClass('album-img').classList.add('img-playing');
-		getClass('album-img-min').classList.add('img-playing');
-		getId('playBtnImg').src = './img/pause.svg';
-	} else {
-		isStop = true
-		getClass('album-img').classList.remove('img-playing');
-		getClass('album-img-min').classList.remove('img-playing');
-		getId('playBtnImg').src = './img/play.svg'
-	}
+  if (flag) {
+    isStop = false
+    getClass('album-img').classList.add('img-playing')
+    getClass('album-img-min').classList.add('img-playing')
+    getId('playBtnImg').src = './img/pause.svg'
+  } else {
+    isStop = true
+    getClass('album-img').classList.remove('img-playing')
+    getClass('album-img-min').classList.remove('img-playing')
+    getId('playBtnImg').src = './img/play.svg'
+  }
 }
 function generateSongInfo() {
-	getId('header-background-img').src = allTracks[currentIndex].img;
-	getClass('album-img').src = allTracks[currentIndex].img;
-	getClass('album-img-min').src = allTracks[currentIndex].img;
-	getId('nameAndSinger').innerText = allTracks[currentIndex].name + '-' + allTracks[currentIndex].singer;
-	getId('album').innerText = allTracks[currentIndex].album;
+  getId('header-background-img').src = allTracks[currentIndex].img
+  getClass('album-img').src = allTracks[currentIndex].img
+  getClass('album-img-min').src = allTracks[currentIndex].img
+  getId('nameAndSinger').innerText = allTracks[currentIndex].name + '-' + allTracks[currentIndex].singer
+  getId('album').innerText = allTracks[currentIndex].album
 }
 
 function next() {
-	playState(false);
-	if(currentPlayMode === 'normal') {
-		if(currentIndex === allTracks.length -1) {
-			currentIndex = 0;
-		}else {
-			currentIndex++;
-		}
-	} else if(currentPlayMode === 'random') {
-		currentIndex = generateRandomIndex(currentIndex);
-	}
-	player.src = allTracks[currentIndex].url;
-	playSong();
+  playState(false)
+  if (currentPlayMode === 'normal') {
+    if (currentIndex === allTracks.length - 1) {
+      currentIndex = 0
+    } else {
+      currentIndex++
+    }
+  } else if (currentPlayMode === 'random') {
+    currentIndex = generateRandomIndex(currentIndex)
+  }
+  player.src = allTracks[currentIndex].url
+  playSong()
 }
 
 function generateRandomIndex(currentIndex) {
-	var newIndex = Math.ceil(Math.random() * allTracks.length -1)
-	if(newIndex === currentIndex) {
-		generateRandomIndex(newIndex);
-	}else {
-		return newIndex;
-	}
+  var newIndex = Math.ceil(Math.random() * allTracks.length - 1)
+  if (newIndex === currentIndex) {
+    generateRandomIndex(newIndex)
+  } else {
+    return newIndex
+  }
 }
 function prev() {
-	playState(false);
-	if(currentPlayMode==='normal'){
-		if(currentIndex === 0){
-			currentIndex = allTracks.length -1;
-		}else {
-			currentIndex --;
-		}		
-	}else if(currentPlayMode === 'random') {
-		currentIndex = Math.ceil(Math.random() * allTracks.length -1)
-	}
-	player.src = allTracks[currentIndex].url;
-	playSong()
+  playState(false)
+  if (currentPlayMode === 'normal') {
+    if (currentIndex === 0) {
+      currentIndex = allTracks.length - 1
+    } else {
+      currentIndex--
+    }
+  } else if (currentPlayMode === 'random') {
+    currentIndex = Math.ceil(Math.random() * allTracks.length - 1)
+  }
+  player.src = allTracks[currentIndex].url
+  playSong()
 }
-var timer = null;
+var timer = null
 
 function changeMode(playMode) {
-	let notice = getId('notification');
-	if(currentPlayMode === playMode) {
-		currentPlayMode = 'normal'
-	}else {
-		currentPlayMode = playMode;
-	}
-	notice.innerText = `Play Mode: ${currentPlayMode}`;
-	notice.style.top = '0';
-	clearTimeout(timer);
-	timer = setTimeout(function() {
-		notice.style.top = '-45px';
-	},1000)
+  let notice = getId('notification')
+  if (currentPlayMode === playMode) {
+    currentPlayMode = 'normal'
+  } else {
+    currentPlayMode = playMode
+  }
+  notice.innerText = `Play Mode: ${currentPlayMode}`
+  notice.style.top = '0'
+  clearTimeout(timer)
+  timer = setTimeout(function () {
+    notice.style.top = '-45px'
+  }, 1000)
 }
 
-function movePlayer (e) {
-	console.log(e)
-	const playerMini = getId('player-min');
-	let top = 0;
-	let left = 0;
-	if(e.clientX >= document.body.clientWidth - 50) {
-		left = document.body.clientWidth - 50;
-	}else if(e.clientX <= 0) {
-		left = 0;
-	}else {
-		left = playerMini.offsetLeft + e.layerX ;
-	}
-	
-	if(e.clientY > document.body.clientHeight - 50) {
-		top = document.body.clientHeight - 50
-	}else if (e.clientY <= 0) {
-		top = 0;
-	}else {
-		top = playerMini.offsetTop + e.layerY 
-	}
-	playerMini.style.top =  top +  'px';
-	playerMini.style.left = left + 'px'
+function movePlayer(e) {
+  const playerMini = getId('player-min')
+  let top = 0
+  let left = 0
+  if (e.clientX >= document.body.clientWidth - 50) {
+    left = document.body.clientWidth - 50
+  } else if (e.clientX <= 0) {
+    left = 0
+  } else {
+    left = playerMini.offsetLeft + e.layerX
+  }
+
+  if (e.clientY > document.body.clientHeight - 50) {
+    top = document.body.clientHeight - 50
+  } else if (e.clientY <= 0) {
+    top = 0
+  } else {
+    top = playerMini.offsetTop + e.layerY
+  }
+  playerMini.style.top = top + 'px'
+  playerMini.style.left = left + 'px'
 }
 
 function stop(e) {
-	e.cancelBubble = true
+  e.cancelBubble = true
 }
 function clickBar(e) {
-	getClass('bar-control').style.left = e.layerX + 'px'
-	var duration = player.duration
-	let percentage = e.layerX / e.path[0].clientWidth
-	var current_m = parseInt(percentage * duration / 60);
-	var current_s = parseInt(percentage * duration % 60);
-	document.getElementById('currentTime').innerText = current_m + ':' + ((current_s < 10) ? '0' + current_s : current_s);
-	player.currentTime = percentage * duration
+  getClass('bar-control').style.left = e.layerX + 'px'
+  var duration = player.duration
+  let percentage = e.layerX / e.path[0].clientWidth
+  var current_m = parseInt((percentage * duration) / 60)
+  var current_s = parseInt((percentage * duration) % 60)
+  document.getElementById('currentTime').innerText = current_m + ':' + (current_s < 10 ? '0' + current_s : current_s)
+  player.currentTime = percentage * duration
+}
+
+getId('player-min').ontouchmove = function (e) {
+  let top = 0
+  let left = 0
+  if (e.touches[0].clientX >= document.body.clientWidth - 50) {
+    left = document.body.clientWidth - 50
+  } else if (e.touches[0].clientX <= 0) {
+    left = 0
+  } else {
+    left = e.touches[0].clientX
+  }
+
+  if (e.touches[0].clientY > document.body.clientHeight - 50) {
+    top = document.body.clientHeight - 50
+  } else if (e.touches[0].clientY <= 0) {
+    top = 0
+  } else {
+    top = e.touches[0].clientY
+  }
+  this.style.top = top + 'px'
+  this.style.left = left + 'px'
 }
